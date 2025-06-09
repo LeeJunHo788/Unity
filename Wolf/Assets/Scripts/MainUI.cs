e8d02bcb2163b7b6d6064d5689df1cbfb9270535
LeeJunHo788 <wnm154@naver.com>
2025-06-09 16:48:54 +0900

    기본UI

--- Wolf/Assets/Scripts/MainUI.cs
+++ Wolf/Assets/Scripts/MainUI.cs
@@ -3,15 +3,35 @@ using UnityEngine.UI;
 
 public class MainUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  [SerializeField]
+  private InputField nameInputField;
+
+  [SerializeField]
+  private GameObject EnterRoomUI;
+
+  Text placeholderText;
+
+  private void Start()
+  {
+    placeholderText = nameInputField.placeholder.GetComponent<Text>();
+  }
+
+  public void OnClickEnterRoomButton()
+  {
+    if (nameInputField.text != "")
     {
-        
+      EnterRoomUI.gameObject.SetActive(true);
+      gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    else
     {
-        
+      placeholderText.text = "이름을 입력해주세요";
+      placeholderText.color = Color.red;
     }
+  }
+
+
+
+    
 }
